19ecac63f5db10a90b16458e566dee0c0f764948
loicmaes <97891718+loicmaes@users.noreply.github.com>
2022-02-09 14:03:47 +0100

    :rocket: Global command system

---
+++ src/main/java/fr/ml/uhcprison/core/commands/UHCPrisonCommand.java
@@ -0,0 +1,20 @@
+package fr.ml.uhcprison.core.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.List;
+
+public class UHCPrisonCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        return null;
+    }
+}
